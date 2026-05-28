18457cc2bcad9a129d5a20eb741d05ade219c0b4
scooter-gh <235403417+scooter-gh@users.noreply.github.com>
2026-05-28 12:18:03 -0400

    fix: use_path_style in backend configs, remove deprecated params

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -7,7 +7,7 @@ terraform {
     skip_credentials_validation = true
     skip_metadata_api_check     = true
     skip_requesting_account_id  = true
-    s3_use_path_style           = true
+    use_path_style              = true
     use_lockfile                = true
   }
 }
