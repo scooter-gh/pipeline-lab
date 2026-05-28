f22aaca4aadd32cab89a806685e1c895fce55ad3
scooter-gh <235403417+scooter-gh@users.noreply.github.com>
2026-05-28 09:27:38 -0400

    fix: add s3_use_path_style for MiniStack compatibility

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -15,6 +15,8 @@ provider "aws" {
   skip_metadata_api_check     = true
   skip_requesting_account_id  = true
 
+  s3_use_path_style            = true
+
   endpoints {
     s3 = "http://ministack:4566"
   }
